6d0b04ef100456603457f7ec949fa937fb4c7283
Electro <electrolitic21@gmail.com>
2024-12-06 17:43:29 -0600

    Update tmux configuration

--- user/shell/tmux.nix
+++ user/shell/tmux.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{pkgs, ...}: {
+  # Note this causes strange things when using my own .tmux.conf
+  # programs.tmux = {
+  # enable = true;
+  # };
 
-{
-  programs.tmux = {
-    enable = true;
-  };
-
+  home.packages = with pkgs; [
+    tmux
+  ];
   home.file.".tmux.conf".source = ./tmux.conf;
 }
-
