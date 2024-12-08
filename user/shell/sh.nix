396413365906bfa50358481a31f034e5989add0b
Electro <electrolitic21@gmail.com>
2024-12-08 09:12:44 -0600

    Update shell configuration

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   # My shell aliases
   myAliases = {
     c = "clear";
@@ -26,9 +25,9 @@ let
     gstap = "git stash pop";
     grep = "grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}";
     l = "ls -lah";
-    la = "ls -lAh";
+    la = "ls -Ah";
     ll = "ls -lh";
-    ls = "ls --color=tty";
+    ls = "ls --color";
     o = "xdg-open";
     v = "nvim";
     vi = "nvim";
@@ -39,50 +38,121 @@ let
     rg = "rg -S"; # Smart case
     ag = "rg";
   };
-in
-{
+in {
   imports = [
     ./tmux.nix
   ];
 
   home.packages = with pkgs; [
-    zsh-powerlevel10k
     fzf
+    zsh-fzf-tab
   ];
 
   programs.zsh = {
     enable = true;
-    autosuggestion = {
-      enable = true;
-    };
-    # historySubstringSearch.enable = true;
+    # history = {
+    #   ignoreAllDups = true;
+    #   ignoreSpace = true;
+    #   save = 10000;
+    #   share = true;
+    # };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
     syntaxHighlighting.enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
     shellAliases = myAliases;
 
+    initExtraFirst = ''
+    '';
     initExtra = ''
+      # zsh-syntax-highlighting, zsh-completions, and zsh-autosuggestions
+      # should already be setup by HM.
       source ${pkgs.fzf}/share/fzf/key-bindings.zsh
       source ${pkgs.fzf}/share/fzf/completion.zsh
-      bindkey -s ^f "tmux-sessionizer\n"
+      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+      source ~/.p10k.zsh
+
+      HISTSIZE=10000
+      HISTFILE=~/.zsh_history
+      SAVEHIST=$HISTSIZE
+      HISTDUP=erase
+      setopt appendhistory
+      setopt sharehistory
+      setopt hist_ignore_space
+      setopt hist_ignore_all_dups
+      setopt hist_ignore_dups
+      setopt hist_save_no_dups
+      setopt hist_find_no_dups
+
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' # Case-insensitive tab-complete
+      zstyle ':completion:*' list-colors "''\${(s.:.)LS_COLORS}" # Colore tab-complete
+      # REMOVE THESE 3 IF ZSH-FZF-TAB IS NOT INSTALLED
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+      bindkey '^[OA' history-search-backward   # Up
+      bindkey '^[[A' history-search-backward   # Up
+
+      bindkey '^[OB' history-search-forward # Down
+      bindkey '^[[B' history-search-forward # Down
+
+      bindkey '\e[1~' beginning-of-line            # Home
+      bindkey '\e[7~' beginning-of-line            # Home
+      bindkey '\e[H'  beginning-of-line            # Home
+      bindkey '\eOH'  beginning-of-line            # Home
+
+      bindkey '\e[4~' end-of-line                  # End
+      bindkey '\e[8~' end-of-line                  # End
+      bindkey '\e[F ' end-of-line                  # End
+      bindkey '\eOF'  end-of-line                  # End
+
+      bindkey '^?'    backward-delete-char         # Backspace
+      bindkey '\e[3~' delete-char                  # Del
+      # bindkey '\e[3;5~' delete-char                # sometimes Del, sometimes C-Del
+      bindkey '\e[2~' overwrite-mode               # Ins
+
+      bindkey '^H'      backward-kill-word         # C-Backspace
+
+      bindkey '5~'      kill-word                  # C-Del
+      bindkey '^[[3;5~' kill-word                  # C-Del
+      bindkey '^[[3^'   kill-word                  # C-Del
+
+      bindkey "^[[1;5H" backward-kill-line         # C-Home
+      bindkey "^[[7^"   backward-kill-line         # C-Home
+
+      bindkey "^[[1;5F" kill-line                  # C-End
+      bindkey "^[[8^"   kill-line                  # C-End
+
+      bindkey '^[[1;5C' forward-word               # C-Right
+      bindkey '^[0c'    forward-word               # C-Right
+      bindkey '^[[5C'   forward-word               # C-Right
+
+      bindkey '^[[1;5D' backward-word              # C-Left
+      bindkey '^[0d'    backward-word              # C-Left
+      bindkey '^[[5D'   backward-word              # C-Left
     '';
+    # bindkey -s ^f "tmux-sessionizer\n"
 
-
-    oh-my-zsh = {
-      enable = true;
-      theme = "refined";
-      plugins = [
-        "git"
-        # "kubectl"
-        # "helm"
-        # "docker"
-      ];
-    };
+    # oh-my-zsh = {
+    #   enable = true;
+    #   theme = "refined";
+    #   plugins = [
+    #     "git"
+    #   ];
+    # };
   };
 
-  home.file.".local/bin/tmux-sessionizer" = {
-    source = ./scripts/tmux-sessionizer;
-    executable = true;
-  };
+  # home.file.".local/bin/tmux-sessionizer" = {
+  #   source = ./scripts/tmux-sessionizer;
+  #   executable = true;
+  # };
 
   programs.bash = {
     enable = true;
@@ -101,4 +171,3 @@ in
   programs.direnv.enableZshIntegration = true;
   programs.direnv.nix-direnv.enable = true;
 }
-
