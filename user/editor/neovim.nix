48098edf6dfc558f9845024480e30e61ecd198ef
Electro <electrolitic21@gmail.com>
2025-06-14 16:22:58 -0500

    neovim: Add cmp_luasnip and cleanup

--- user/editor/neovim.nix
+++ user/editor/neovim.nix
@@ -1,45 +1,44 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # Faster grep, like silver searcher (ag)
-    ripgrep
+    alejandra # Nix formatter
     lua-language-server
     nil # Nix LSP
-    alejandra # Nix formatter
     pyright
+    ripgrep # Faster grep, like silver searcher (ag)
   ];
 
   programs.neovim = {
     enable = true;
-    plugins = [
-      pkgs.vimPlugins.which-key-nvim
-      pkgs.vimPlugins.lz-n
-      pkgs.vimPlugins.cmp-nvim-lsp
-      pkgs.vimPlugins.cmp-nvim-lsp
-      pkgs.vimPlugins.lspkind-nvim # VSCode-like pictograms for LSP
-      pkgs.vimPlugins.nvim-cmp
-      pkgs.vimPlugins.gitsigns-nvim
-      pkgs.vimPlugins.neogit
-      pkgs.vimPlugins.luasnip
-      pkgs.vimPlugins.friendly-snippets
-      pkgs.vimPlugins.telescope-nvim
-      pkgs.vimPlugins.telescope-ui-select-nvim
-      pkgs.vimPlugins.rainbow-delimiters-nvim
-      pkgs.vimPlugins.precognition-nvim
-      pkgs.vimPlugins.oil-nvim
-      pkgs.vimPlugins.trouble-nvim
-      pkgs.vimPlugins.nvim-surround
-      pkgs.vimPlugins.markdown-nvim
-      pkgs.vimPlugins.nabla-nvim
-      pkgs.vimPlugins.lualine-nvim
-      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-      pkgs.vimPlugins.indent-blankline-nvim
-      pkgs.vimPlugins.noice-nvim
-      pkgs.vimPlugins.onedark-nvim
-      pkgs.vimPlugins.none-ls-nvim
-      pkgs.vimPlugins.nvim-lspconfig
-      pkgs.vimPlugins.crates-nvim
-      pkgs.vimPlugins.vim-tmux-navigator
-      pkgs.vimPlugins.nvim-autopairs
+    plugins = with pkgs.vimPlugins; [
+      cmp-nvim-lsp
+      cmp_luasnip
+      crates-nvim
+      friendly-snippets
+      gitsigns-nvim
+      indent-blankline-nvim
+      lspkind-nvim # VSCode-like pictograms for LSP
+      lualine-nvim
+      luasnip
+      lz-n
+      markdown-nvim
+      nabla-nvim
+      neogit
+      noice-nvim
+      none-ls-nvim
+      nvim-autopairs
+      nvim-cmp
+      nvim-lspconfig
+      nvim-surround
+      nvim-treesitter.withAllGrammars
+      oil-nvim
+      onedark-nvim
+      precognition-nvim
+      rainbow-delimiters-nvim
+      telescope-nvim
+      telescope-ui-select-nvim
+      trouble-nvim
+      vim-tmux-navigator
+      which-key-nvim
     ];
   };
 }
