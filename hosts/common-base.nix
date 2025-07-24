466e9f1d32df133b21416bd0e119ccde10184512
Electro <electrolitic21@gmail.com>
2025-07-24 14:43:56 -0500

    Update flake.lock

--- hosts/common-base.nix
+++ hosts/common-base.nix
@@ -20,17 +20,19 @@
   time.timeZone = systemSettings.timezone;
 
   # Select internationalization properties.
-  i18n.defaultLocale = systemSettings.locale;
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = systemSettings.locale;
-    LC_IDENTIFICATION = systemSettings.locale;
-    LC_MEASUREMENT = systemSettings.locale;
-    LC_MONETARY = systemSettings.locale;
-    LC_NAME = systemSettings.locale;
-    LC_NUMERIC = systemSettings.locale;
-    LC_PAPER = systemSettings.locale;
-    LC_TELEPHONE = systemSettings.locale;
-    LC_TIME = systemSettings.locale;
+  i18n = {
+    defaultLocale = systemSettings.locale;
+    extraLocaleSettings = {
+      LC_ADDRESS = systemSettings.locale;
+      LC_IDENTIFICATION = systemSettings.locale;
+      LC_MEASUREMENT = systemSettings.locale;
+      LC_MONETARY = systemSettings.locale;
+      LC_NAME = systemSettings.locale;
+      LC_NUMERIC = systemSettings.locale;
+      LC_PAPER = systemSettings.locale;
+      LC_TELEPHONE = systemSettings.locale;
+      LC_TIME = systemSettings.locale;
+    };
   };
 
   nix.settings = {
