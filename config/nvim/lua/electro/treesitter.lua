81ed3a3a712814d8d520d293825aecb857ffab06
Electro <electrolitic21@gmail.com>
2026-01-17 17:14:50 -0600

    Fix broken tree-sitter plugin

--- config/nvim/lua/electro/treesitter.lua
+++ config/nvim/lua/electro/treesitter.lua
@@ -1,13 +1,7 @@
----@diagnostic disable: missing-fields
-require("nvim-treesitter").setup({
-  ensure_installed = {},
-  auto_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = true, },
-  incremental_selection = {
-    enable = true,
-  },
+require("nvim-treesitter").setup({})
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
 })
